f838f58db79ce9d66be6d50293b50ef2852ae5b7
junyuxu2006 <55368471+junyuxu2006@users.noreply.github.com>
2020-09-12 15:32:07 -0500

    Add files via upload

---
+++ config.example.py
@@ -0,0 +1,14 @@
+configs = {\
+'token' : 'Token here',\
+'link' : 'https://(DOMAIN NAME HERE)/wp-login.php?redirect_to=(POST LINK HERE)',\
+'verifiedrolename' : 'Walker',\
+'unverifiedrolename': 'Unverified',\
+'controlchannelname' : 'name here',\
+'controlroomID' : 'ID here',\
+'WelcomeChannel' : 'ID here',\
+'PATH' : "PATH to webdriver here. (use double slash instead of slash",\
+'email' : "Your email for the account on the platform",\
+'password' : "Your password for the account on the platform",\
+'commenturl' : "URL of the post",\
+'Invitelink' : 'Invite link for the bot'
+}

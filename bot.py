b8e8b19fc30ec6405b2ddb1f87b9e8b858e49d4f
junyuxu2006 <55368471+junyuxu2006@users.noreply.github.com>
2020-09-13 05:02:55 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,6 +15,7 @@ import random
 from config import configs
 from discord.ext import commands
 from discord.utils import get
+import asyncio
 PATH = configs['PATH']
 link = configs['link']
 email = configs['email']
@@ -22,10 +23,10 @@ password = configs['password']
 verifiedrolename = configs['verifiedrolename']
 unverifiedrolename = configs['unverifiedrolename']
 Invitelink = configs['Invitelink']
+prefix = configs['prefix']
+verificationchannelname = configs['verificationchannelname']
 
-
-
-client = discord.Client()
+client = commands.Bot(command_prefix = prefix)
 commenturl = configs['commenturl']
 token = configs['token']
 verifiedrole = configs['verifiedrolename']
@@ -35,32 +36,61 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 
-
+client.remove_command('help')
 client.commentToken = ""
-@client.event
-async def on_message(message):
-    member = message.author
+@client.command()
+async def verify(ctx):
+    WalkerID = {}
+    member = ctx.author
     DiscordID = member.id
-    if message.content.startswith("$verify"):
-        token = hex(DiscordID)[2:]
-        client.commentToken = token[:8] + secrets.token_urlsafe(6) + token[8:]
-        stringToken = str(client.commentToken)
-        await message.channel.send("NOTE: This discord bot is **still in development, if you experience errors, please contact Walker #7416.** Please make a comment on the following post exactly as the token below. Then, type `$go WalkerID(NO #)`, where WalkerID is YOUR OWN WALKER ID."+" <"+str(commenturl) + "> ")
-        await message.channel.send("copy the token below and paste it in the comments section of the post.")
-        await message.channel.send(stringToken)
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello.')
-    if message.content.startswith('$invite'):
-        await message.channel.send("Invite me here! <"+Invitelink+">")
-    if message.content.startswith('$help'):
-        await message.channel.send("`$help` - list all the commands.")
-        await message.channel.send("`$invite` - Invite link for the bot.")
-        await message.channel.send("`$hello` - Bot will say Hello.")
-        await message.channel.send("`$verify` - Shows instructions for verification, and generates a token to comment on.")
-        await message.channel.send("`$go` - The command you type followed by your Walker ID for comment checking purposes.")
-        await message.channel.send("`$ungo` - The command you type to unverify.")
-    if message.content.startswith("$go "):
-        await message.channel.send("Thanks, I'm checking your comment.")
+    WalkerID[DiscordID] = ctx.message.content[4:]
+    token = hex(DiscordID)[2:]
+    client.commentToken = {}
+    client.commentToken[DiscordID] = token[:8] + secrets.token_urlsafe(6) + token[8:]
+    stringToken = str(client.commentToken[DiscordID])
+    await ctx.channel.send("NOTE: This discord bot is **still in development, if you experience errors, please contact Walker #7416.** Please make a comment on the following post exactly as the token below. Then, type `$go WalkerID(NO #)`, where WalkerID is YOUR OWN WALKER ID."+" <"+str(commenturl) + "> ")
+    await ctx.channel.send("copy the token below and paste it in the comments section of the post.")
+    await ctx.channel.send(stringToken)
+
+@commands.cooldown(1, 30, commands.BucketType.user)
+@client.command()
+async def hello(ctx):
+        await ctx.channel.send('Hello.')
+
+@commands.cooldown(1, 30, commands.BucketType.user)
+@client.command()
+async def invite(ctx):
+        await ctx.channel.send("Invite me here! <"+Invitelink+">")
+
+@client.command()
+async def help(ctx):
+        embed=discord.Embed(title="Help", color=0x0400ff)
+        embed.add_field(name="$help", value="shows this message.", inline=True)
+        embed.add_field(name="$invite", value="Invite link for the bot.", inline=True)
+        embed.add_field(name="$hello", value="Bot will say Hello.", inline=True)
+        embed.add_field(name="$verify", value="Shows instructions for verification, and generates a token to comment on.", inline=True)
+        embed.add_field(name="$go", value="The command you type followed by your Walker ID for comment checking purposes.", inline=True)
+        embed.add_field(name="$ungo", value="The command you type to unverify.", inline=True)
+        embed.add_field(name="$say", value="The command you type followed by your desired message to make the bot say that.", inline=True)
+        embed.set_footer(text="Noice")
+        await ctx.channel.send(embed=embed)
+WalkerIDFound = {}
+TokenFound = {}
+@client.command()
+async def say (ctx):
+    try:
+        Content = ctx.message.content[5:]
+        await ctx.message.channel.purge(limit = 1)
+        await ctx.channel.send(Content)
+    except AttributeError:
+        await ctx.channel.send("This command does not support DM.")
+@client.command()
+async def go (ctx):
+        member = ctx.author
+        DiscordID = member.id
+        WalkerID = {}
+        WalkerID[DiscordID] = ctx.message.content[4:]
+        await ctx.channel.send("Thanks, I'm checking your comment.")
         driver = webdriver.Chrome(PATH)
         driver.get(link)
         print(driver.title)
@@ -69,46 +99,66 @@ async def on_message(message):
         inputpassword = driver.find_element_by_id("user_pass")
         inputpassword.send_keys(password)
         inputpassword.send_keys(Keys.ENTER)
-        WalkerID = message.content[4:]
-        print(WalkerID)
+        WalkerID[DiscordID] = ctx.message.content[4:]
+        print(WalkerID[DiscordID])
         #xpathWalkerID = str("//*[contains(text(), '"+WalkerID+"'"+")]")
         #xpathToken = str("//*[contains(text(), '"+client.commentToken+"'"+")]")
         WalkerIDelement = driver.find_element_by_xpath("/html/body/div[5]/div/div[1]/main/section/ul/li[last()]/div/div[1]/a/span")
         #driver.find_elements_by_class_name("wid")
         Tokenelement = driver.find_element_by_xpath("/html/body/div[5]/div/div[1]/main/section/ul/li[last()]/div/p")
         #driver.find_elements_by_class_name("comment-author vcard")
-        WalkerIDFound = WalkerIDelement.get_attribute('textContent')
-        TokenFound = Tokenelement.get_attribute('textContent')
-        await message.channel.send("Found ID: "+WalkerIDFound)
-        time.sleep(1)
-        await message.channel.send("Found Token: "+TokenFound)
-        time.sleep(1)
-        await message.channel.send("Entered ID: "+WalkerID)
-        time.sleep(1)
-        await message.channel.send("Generated Token: "+client.commentToken)
-        time.sleep(1)
-    if message.content.startswith('$ungo'):
+        WalkerIDFound = {}
+        TokenFound = {}
+        WalkerIDFound[DiscordID] = WalkerIDelement.get_attribute('textContent')
+        TokenFound[DiscordID] = Tokenelement.get_attribute('textContent')
+        await ctx.channel.send("Found ID: "+WalkerIDFound[DiscordID])
+        await asyncio.sleep(1)
+        await ctx.channel.send("Found Token: "+TokenFound[DiscordID])
+        await asyncio.sleep(1)
+        await ctx.channel.send("Entered ID: "+WalkerID[DiscordID])
+        await asyncio.sleep(1)
+        await ctx.channel.send("Generated Token: "+client.commentToken[DiscordID])
+        await asyncio.sleep(1)
+        TokenFound[DiscordID] = Tokenelement.get_attribute('textContent')
+        if TokenFound[DiscordID] == client.commentToken[DiscordID] and WalkerIDFound[DiscordID] == str(WalkerID[DiscordID]):
+            await asyncio.sleep(2)
+            await ctx.channel.send("Verified.")
+            driver.quit()
+            role = get(member.guild.roles, name = verifiedrolename)
+            unrole = get(member.guild.roles, name = unverifiedrolename)
+            WalkerIDnick = "#"+str(WalkerID[DiscordID])
+            try:
+                await member.add_roles(role)
+                await member.remove_roles(unrole)
+                await member.edit(nick=WalkerIDnick)
+            except discord.errors.Forbidden:
+                await ctx.channel.send("I do not have permissions to add/remove roles and/or change your nickname, please contact your Admin or owner.")
+            print(DiscordID)  
+        else:
+            await asyncio.sleep(2)
+            await ctx.channel.send("Could not find your comment, if you did comment, please make sure you commented the token in the black box below the link and you entered the correct Walker ID.")
+            driver.quit()
+@commands.cooldown(1, 30, commands.BucketType.user)
+@client.command()
+async def ungo(ctx):
+        member = ctx.author
         role = get(member.guild.roles, name = verifiedrolename)
         unrole = get(member.guild.roles, name = unverifiedrolename)
         await member.add_roles(unrole)
         await member.remove_roles(role)
-        await message.channel.send("Unverified.")
-    if TokenFound == client.commentToken and WalkerIDFound == WalkerID:
-        time.sleep(2)
-        await message.channel.send("Verified.")
-        driver.quit()
-        role = get(member.guild.roles, name = verifiedrolename)
-        unrole = get(member.guild.roles, name = unverifiedrolename)
-        await member.add_roles(role)
-        await member.remove_roles(unrole)
-        WalkerIDnick = "#"+WalkerID
-        await member.edit(nick=WalkerIDnick)
-        print(DiscordID)  
-    else:
-        time.sleep(2)
-        await message.channel.send("Could not find your comment, if you did comment, please make sure you commented the token in the black box below the link and you entered the correct Walker ID.")
-        driver.quit()
+        await ctx.channel.send("Unverified.")
+
+
 @client.event
 async def on_guild_join(guild):
-    await random.choice(guild.text_channels).send('Thanks for adding me. In order for me to properly function, make sure you have a role named "'+verifiedrolename+'" and "'+unverifiedrolename+'". My role has to be above them.')
+    await random.choice(guild.text_channels).send('Thanks for adding me. In order for me to properly function, make sure you have a role named "'+verifiedrolename+'" and "'+unverifiedrolename+'", and make sure my role is above them. Your server must have a public channel which I can send messages that is named "'+verificationchannelname+'.')
+@client.event
+async def on_member_join(member):
+    unrole = get(member.guild.roles, name = unverifiedrolename)
+    channel = discord.utils.get(member.guild.text_channels, name = verificationchannelname)
+    await member.add_roles(unrole)
+    try:
+        await channel.send(f"Hey {member.mention}, welcome to **{member.guild.name}**, please type `$verify` to get started on your verification process.")
+    except AttributeError:
+        await random.choice(member.guild.text_channels).send(f"Hey {member.mention}, welcome to **{member.guild.name}**, please type `$verify` to get started on your verification process.")
 client.run(token)

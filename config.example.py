c47cbfed3af899bf3898d34441b82a03ca39c6e0
Junyu Xu <55368471+junyuxu2006@users.noreply.github.com>
2020-09-23 22:57:57 -0500

    Update config.example.py

--- config.example.py
+++ config.example.py
@@ -15,4 +15,5 @@ configs = {\
 'logchannelname' : 'verification-bot-log',\
 'generalchannelname' : 'general',\
 'url' : 'URL to the wordpress API link to comments for accessing comments',\
+'allposts' : 'URL to all posts section of the site',\
 }

bd63a5698d764418645ff569ee427725f495908d
Junyu Xu <55368471+junyuxu2006@users.noreply.github.com>
2020-09-29 07:34:56 -0500

    Update login.py

--- login.py
+++ login.py
@@ -2,8 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 from config import configs
-URL = configs['commenturl']
-def login(login):
+searchlink = configs['searchlink']
+def login(login, URL):
     session = requests.Session()
 
 
@@ -20,10 +20,23 @@ def login(login):
     data["pwd"] = login[1]
     postURL = urljoin(URL, form["action"])
     r = session.post(postURL, data=data)
+    print(r)
+    print(type(r))
+    if str(r) == "<Response [401]>":
+        print("Error 401")
+        captcha(soup)
     return r
 
-def comments(user):
-    r = login(user)
+def captcha(html):
+    print("got in the captcha function")
+    question = html.find("span", {"style": "vertical-align:super;"})
+    print("got the question")
+    print(question)
+    nospace = str(question).replace(" ","")
+    print(nospace)
+
+def comments(user, URL):
+    r = login(user, URL)
 
     html = BeautifulSoup(r.text, "html.parser")
 
@@ -35,3 +48,54 @@ def comments(user):
         tokens_list.append([found_id,found_token])
 
     return tokens_list
+
+def post(user,URL):
+    r = login(user,URL)
+
+    html = BeautifulSoup(r.text, "html.parser")
+
+    posts = html.findAll("a", {"class": "nolink"})
+    post_list = []
+    for i,post in enumerate(posts):
+        if i != 0:
+            post_link = post['href']
+            h2 = post.find("h2")
+            texts  = str(h2)[str(h2).find(">")+1:]
+            texts  = texts[str(texts).find(">")+1:]
+            author = texts[0:str(texts).find("<")]
+            texts = texts[str(texts).find(">")+1:]
+            title = texts[0:str(texts).find("<")]
+            title = title.replace(" - ", "")
+            post_list.append([author,title,post_link])       
+    return post_list
+
+def post_contents(user, URL):
+    r = login(user,URL)
+
+    html = BeautifulSoup(r.text, "html.parser")
+
+    nameIT = html.find("article", {"id": "forum-single"}).text
+    description = nameIT.replace("Report", "")
+    return description
+
+def searchPost(user, toSearch, max=10):
+    r = login(user,searchlink+toSearch
+    html = BeautifulSoup(r.text, "html.parser")
+    posts = html.findAll("a", {"class": "nolink"})
+    post_list = []
+    
+    for i,post in enumerate(posts):
+        if i != 0:
+            post_link = post['href']
+            h2 = post.find("h2")
+            texts  = str(h2)[str(h2).find(">")+1:]
+            texts  = texts[str(texts).find(">")+1:]
+            author = texts[0:str(texts).find("<")]
+            texts = texts[str(texts).find(">")+1:]
+            title = texts[0:str(texts).find("<")]
+            title = title.replace(" - ", "")
+            post_list.append([author,title,post_link])
+            if i == max:
+                return post_list    
+    return post_list
+            
